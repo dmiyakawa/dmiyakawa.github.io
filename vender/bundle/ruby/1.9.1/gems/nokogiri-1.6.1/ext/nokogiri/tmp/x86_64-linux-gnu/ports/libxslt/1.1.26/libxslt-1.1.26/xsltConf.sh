30470cccac7e1144cca359ee14928400a0526003
Daisuke Miyakawa <d.miyakawa@gmail.com>
2014-05-02 16:41:14 +0900

    Remove amazon-ecs

--- vender/bundle/ruby/1.9.1/gems/nokogiri-1.6.1/ext/nokogiri/tmp/x86_64-linux-gnu/ports/libxslt/1.1.26/libxslt-1.1.26/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/dmiyakawa/Documents/octopress/vender/bundle/ruby/1.9.1/gems/nokogiri-1.6.1/ports/x86_64-linux-gnu/libxslt/1.1.26/lib"
-XSLT_LIBS="-lxslt  -L/home/dmiyakawa/Documents/octopress/vender/bundle/ruby/1.9.1/gems/nokogiri-1.6.1/ports/x86_64-linux-gnu/libxml2/2.8.0/lib -lxml2 -lz -lm -lm"
-XSLT_INCLUDEDIR="-I/home/dmiyakawa/Documents/octopress/vender/bundle/ruby/1.9.1/gems/nokogiri-1.6.1/ports/x86_64-linux-gnu/libxslt/1.1.26/include"
-MODULE_VERSION="xslt-1.1.26"
